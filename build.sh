f16728cb1f2230815d6ab350e02302ed756321be
Pratyush Poddar <pratyush.poddar@ymedialabs.com>
2016-06-27 16:05:35 +0530

    update build

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@ NODE_ENV=production webpack -p --config webpack.production.config.js
 git status
 git add .
 git commit -m "update build"
-git fetch origin
+git fetch --all
 git merge origin/master
 git push origin master
-git push origin release
